121f5045df4b4b94aa62b1c94ccfc12d32a0c33d
xfokiux xfokiux <xfokiuxxfokiux@192.168.1.9>
2024-11-27 00:36:50 -0500

    footer responsive update

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -1,33 +1,39 @@
+import { FacebookIcon,Instagram, InstagramIcon } from "lucide-react"
+
 export default function Footer() {
   return (
     <footer className="border-2 border-yellow-400  bg-black text-white md:mt-44 ">     
-        <div className="border-2 border-lime-300 m-2 md:p-44">
+        <div className="border-4 border-blue-500 m-2 md:m-44 md:p-4 text-center">
           <div>
-            <h1 className="text-blue-500 text-4xl font-black">STAY UPDATE</h1>
+            <h1 className="text-blue-500 m-5 text-4xl font-black md:text-7xl">STAY UPDATE</h1>
           </div>
           <div>
             <form action="">
-            <div>
+            <div className="flex flex-col justify-center">
               <label htmlFor="email">Enter your email here*</label>
-              <input type="text" />              
+              <input type="text" className=" md:w-80 ml-auto mr-auto" />              
             </div>
           </form>
           </div>
           
           
             <div>
-              <div>
-              <input type="checkbox" name="" id="" />
-                <p>yes, subcribe me to your newsletter.</p>
+              <div className="flex justify-center gap-3">
+            <input type="checkbox" name="" id="" />
+            <p>yes, subcribe me to your newsletter.</p>
             </div>
-            <div>
+            <div className="mt-5 mb-5">
               <button className="bg-blue-500 text-black font-extrabold pl-10 pr-10 pt-5 pb-5">Subcribe Now</button>
           </div>
-          <div>
+          <div className="flex flex-col-reverse justify-center gap-2">
+            <div>
             <div>tel:234435344</div>
-            <div>email:tehuthuthe@thethet.com</div>
-            <div>facebook</div>
-            <div>instagram</div>
+              <div>email:tehuthuthe@thethet.com</div>
+            </div>
+            <div className="flex justify-center">
+            <div className=" cursor-pointer"><FacebookIcon /></div>
+            <div className=" cursor-pointer"><InstagramIcon /></div>
+            </div>
           </div>
           <div>
             <div className="text-blue-500">© 2025 by Satoribjj</div>
